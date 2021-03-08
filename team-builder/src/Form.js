775f13a49da02489d638635d1c64fb7033797ea9
lateralcreativity <lateralcreativity@outlook.com>
2021-03-08 10:31:42 -0600

    mvp

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,6 +1,9 @@
 
 import {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+
 function Form(props) {
+    let history = useHistory();
     const addMember = props.addMember;
     const initialFormValues = {
         name: '',
@@ -19,22 +22,24 @@ function Form(props) {
         event.preventDefault();
 
         addMember(formValues);
+        setFormValues(initialFormValues);
+        history.push('/');
     }
     return (
-        <form action="">
+        <form action="" onSubmit={submitHandler}>
             <label htmlFor="">
                 Name:
                 <input type="text" name="name" onChange={inputHandler}/>
             </label>
             <label htmlFor="">
                 Email:
-                <input type="text" email="email" />
+                <input type="text" name="email" onChange={inputHandler}/>
             </label>
             <label htmlFor="">
                 Role:
-                <input type="text" role="role" />
+                <input type="text" name="role" onChange={inputHandler}/>
             </label>
-            <input type="submit" value="submit" onSubmit={submitHandler}/>
+            <input type="submit" value="submit"/>
         </form>
     );
 }
